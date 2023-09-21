4decf4cab1bca6e20f6f5ca24a4d9a4708b9fa22
Oscar <oscarcheruiyot3@gmail.com>
2023-09-21 09:30:53 +0300

    Making minimal modifications

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -9,32 +9,33 @@ const Contact = () => {
                 <p className='mb-10 text-center'>"Books are the quietest and most constant of friends; they are the most accessible and wisest of counselors, <br />and the most patient of teachers."  <span className='text-sm'>- Charles W. Eliot</span> </p>
                 <div className='p-3 m-3 bg-slate-600 rounded-md'>
                     <h1> Get in touch for Hard or Soft Copies of the books</h1>
-                    <p className='text-sm italic mb-4'>NB: Inquire first if the book is available for the  hard copies</p>
+                    <p className='text-sm italic mb-4'>NB: Inquire first if the book is available for the  hard copies </p>
+                    <p className='text-sm text-sky-500 italic'>The price of a softcopy ranges between Ksh 80 - 200 and the hard copy depends on the book</p>
                     <div className='flex gap-5 justify-center mt-4'>
                         <img src='/phone.png' alt='' width={30} height={30} />
-                        <p>0747770747</p>
+                        <p>0768862624</p>
 
                     </div>
                     <div className='flex gap-5 justify-center mt-4'>
                         <img src='/email1.png' alt='' width={30} height={30} />
-                        <p>bookhub99@gmail.com</p>
+                        <p>bookhub254@gmail.com</p>
 
                     </div>
                     <div className='flex gap-5 justify-center mt-4'>
                         <img src='/whatsapp.png' alt='' width={30} height={30} />
-                        <p>0747770747</p>
+                        <p>0768862624</p>
 
                     </div>
                     <div className='flex gap-5 justify-center mt-4'>
                         <img src='/telegram.png' alt='' width={30} height={30} />
-                        <p>0747770747</p>
+                        <p>0768862624</p>
 
                     </div>
 
                 </div>
 
             </form>
-            Contact
+  
 
         </div>
     )
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className='flex flex-col bg-opacity-60 bg-slate-200 shadow-xl justify-center bottom-0 fixed '>
         <img src='./book.png' alt='' width={200} height={70} className='transparent'/>
-        <p className='text-sm ml-[30px]'>Contact: 0747770747</p>
+        <p className='text-sm ml-[30px]'>Contact: 0768862624</p>
 
     </div>
   )
